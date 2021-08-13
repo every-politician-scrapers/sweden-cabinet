8e696e7911d4f2bf911c7e06514371c0484a69df
Tony Bowden <tony@tmtm.com>
2021-08-13 15:06:04 +0300

    Bump version to 0.1.1

--- add-p39.js
+++ add-p39.js
@@ -2,25 +2,13 @@ const fs = require('fs');
 let rawmeta = fs.readFileSync('meta.json');
 let meta = JSON.parse(rawmeta);
 
-module.exports = (id, position, personname, positionname, startdate) => {
-  reference = {
-    P854: meta.source.url,
-    P1476: {
-      text: meta.source.title,
-      language: meta.source.lang.code,
-    },
-    P813: new Date().toISOString().split('T')[0],
-    P407: meta.source.lang.wikidata,
-  }
-
+module.exports = (id, position, startdate) => {
   qualifier = {
     P580: meta.cabinet.start,
     P5054: meta.cabinet.id,
   }
 
   if(startdate)      qualifier['P580']  = startdate
-  if(personname)     reference['P1810'] = personname
-  if(positionname)   reference['P1932'] = positionname
 
   return {
     id,
@@ -28,7 +16,6 @@ module.exports = (id, position, personname, positionname, startdate) => {
       P39: {
         value: position,
         qualifiers: qualifier,
-        references: reference,
       }
     }
   }
